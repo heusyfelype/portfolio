cd653f9851b1dbe04b17e3bfedd8dc389744a532
heusyfelype <felype.heusy@gmail.com>
2023-09-09 20:06:47 -0300

    release: projeto iniciado

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+const body = document.querySelector("body") 
+
+body?.addEventListener("scroll", (e) =>{
+  console.log("Evento: ", e);
+  console.log("Window: ", window)
+})
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  position: sticky;
+  top: 0;
+  left: 0;
+  background-color: aqua;
+}
+
+
+nav {
+}
+
+nav a.router-link-exact-active {
+}
+
+nav a.router-link-exact-active:hover {
+}
+
+nav a {
+}
+
+nav a:first-of-type {
+}
+
+@media (min-width: 1024px) {
+  header {
+  }
+
+  header .wrapper {
+  }
+
+  nav {
+  }
+}
+</style>
---
+++ src/views/HomePage/HomeView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <section class="first-section">Primeira seção</section>
+    <section class="second-section">Segunda seção</section>
+  </main>
+</template>
+
+<style>
+main {
+  height: 100vh;
+  width: 80%;
+  overflow-y: scroll;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow: scroll;
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* Internet Explorer / Edge */
+}
+main::-webkit-scrollbar {
+  width: 0px;
+  /* background: transparent;  */
+}
+
+section {
+  height: 100vh;
+  scroll-snap-align: start;
+  scroll-snap-stop: always;
+}
+
+.first-section {
+  background-color: burlywood;
+}
+
+.second-section {
+  background-color: aquamarine;
+}
+</style>
